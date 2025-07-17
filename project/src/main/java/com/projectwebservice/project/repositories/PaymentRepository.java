78af071ee6457f75ee14bebae1361a2beeb5c132
Maycon Vieira <mayconguill@gmail.com>
2025-07-17 09:15:07 -0300

    User insert

--- project/src/main/java/com/projectwebservice/project/repositories/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.projectwebservice.project.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projectwebservice.project.entities.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long>{
-}
